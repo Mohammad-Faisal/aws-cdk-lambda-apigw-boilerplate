93665296b60fa47e9f4445f8f1dc5887ea00027e
Mohammad Faisal <mohammadfaisal1011@gmail.com>
2022-05-23 22:00:12 +0600

    Updated

--- lib/PipelineStack.ts
+++ lib/PipelineStack.ts
@@ -17,30 +17,31 @@ export class PipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: PipelineStackProps) {
     super(scope, id, props);
 
+    // const pipeline =
     const pipeline = new CodePipeline(this, "Pipeline", {
       pipelineName: "TestPipeline",
       synth: new ShellStep("Synth", {
         input: CodePipelineSource.gitHub(
           "Mohammad-Faisal/aws-cdk-lambda-apigw-boilerplate",
-          "master",
-          {
-            authentication: SecretValue.secretsManager("github-token-new"),
-          }
+          "master"
+          // {
+          //   authentication: SecretValue.secretsManager("github-token-new"),
+          // }
         ),
         commands: ["npm ci", "npm run build", "npx cdk synth"],
       }),
     });
 
-    const testingStage = pipeline.addStage(
+    const devStage = pipeline.addStage(
       new AppStage(this, "dev", {
         env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: "us-east-1" },
       })
     );
 
-    testingStage.addPre(
+    devStage.addPre(
       new ShellStep("Run Unit Tests", { commands: ["npm install", "npm test"] })
     );
-    testingStage.addPost(
+    devStage.addPost(
       new ManualApprovalStep("Manual approval before production")
     );
 
