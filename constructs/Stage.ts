451168d8df54346f9ca710769899248aabac970d
Mohammad Faisal <mohammadfaisal1011@gmail.com>
2022-05-23 23:17:19 +0600

    Removed environment

--- constructs/Stage.ts
+++ constructs/Stage.ts
@@ -8,13 +8,9 @@ export class AppStage extends cdk.Stage {
   constructor(scope: Construct, stageName: string, props?: cdk.StageProps) {
     super(scope, stageName, props);
 
-    const authStack = new AuthenticationStack(
-      this,
-      `${stageName}-AuthenticationService`,
-      {
-        stageName,
-      }
-    );
+    const authStack = new AuthenticationStack(this, `AuthenticationService`, {
+      stageName,
+    });
 
     this.urlOutput = authStack.urlOutput;
   }
